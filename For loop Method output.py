9d0ace8a51f0d041a41ed25ee2d0273a78a5f1b1
Amala15 <63283936+Amala15@users.noreply.github.com>
2020-04-07 19:59:38 +0530

    Add files via upload

---
+++ For loop Method output.py
@@ -0,0 +1,6 @@
+Python 3.8.2 (tags/v3.8.2:7b3ab59, Feb 25 2020, 22:45:29) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+=================================================== RESTART: F:/Amala Varghese/My Captain-Online Course/AI (April Course)/Input list 1.py ==================================================
+11 0 45 66 
+>>> 
